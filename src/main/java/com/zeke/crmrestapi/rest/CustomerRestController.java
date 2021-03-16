da715ee00220966b9c46a231bbbffae75c6d6b5d
Foltut Daniel <danfoltut@gmail.com>
2021-03-16 21:20:56 +0200

    Added full functionality and tested with postman

--- src/main/java/com/zeke/crmrestapi/rest/CustomerRestController.java
+++ src/main/java/com/zeke/crmrestapi/rest/CustomerRestController.java
@@ -3,8 +3,12 @@ package com.zeke.crmrestapi.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,9 +33,36 @@ public class CustomerRestController {
 		Customer theCustomer = customerService.getCustomer(customerId);
 		if(theCustomer == null) {
 			throw new CustomerNotFoundException("Customer id is not found- " + customerId);
-		}
-		
+		}		
 		
 		return theCustomer;
+	}
+	
+	@PostMapping("/customers")
+	public Customer addCustomer(@RequestBody Customer theCustomer) {
+		
+		theCustomer.setId(0); //key is empty(0)-> insert new customer / key is full perform update
+		customerService.saveCustomer(theCustomer);
+		
+		return theCustomer;
+	}
+	
+	@PutMapping("/customers")
+	public Customer updateCustomer(@RequestBody Customer theCustomer) {
+		customerService.saveCustomer(theCustomer);
+		
+		return theCustomer;
+	}
+	
+	@DeleteMapping("/customers/{customerId}")
+	public String deleteCustomer(@PathVariable int customerId) {
+		Customer theCustomer = customerService.getCustomer(customerId);
+		if(theCustomer == null) {
+			throw new CustomerNotFoundException("Customer id not found - "+customerId);
+		}
+		
+		customerService.deleteCustomer(customerId);
+		
+		return "Delete customer id -" + customerId;
 	}
 }
